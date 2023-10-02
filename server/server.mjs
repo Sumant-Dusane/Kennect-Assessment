fbc42f9342c1e3ff1fd032e11356e30e23a66d0d
Sumant-Dusane <sumant.dusane1966@gmail.com>
2023-10-02 14:45:10 +0530

    dot env fix

--- server/server.mjs
+++ server/server.mjs
@@ -3,10 +3,10 @@ import cors from "cors";
 import routes from "./routes.mjs";
 import dotenv from "dotenv";
 
+dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
-app.use(dotenv.config());
 app.use(express.json());
 app.use("/api/v1", routes);
 app.listen(PORT, () => {
